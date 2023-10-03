482ef240f1ccb62f30f3f6aa53f2df9f49c1eb1c
Banja David Lotigo <innocdavid@gmail.com>
2023-10-03 19:03:43 +0000

    feature - add a delete user route

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,10 +2,11 @@ import express from 'express';
 
 const userRouter = express.Router();
 
-import { signup , getUser, updateUser } from '../controllers/userController.js';
+import { signup , getUser, updateUser, deleteUser } from '../controllers/userController.js';
 
 userRouter.post('/signup', signup);
 userRouter.get('/user/:id', getUser);
 userRouter.put('/user/:id', updateUser);
+userRouter.delete('/user/:id', deleteUser);
 
 export default userRouter;
