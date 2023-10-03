4b6314b01c8fa3bb2a8759d53bf96866e1ca4adc
Banja David Lotigo <innocdavid@gmail.com>
2023-10-03 18:56:25 +0000

    feature - add update user route

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,9 +2,10 @@ import express from 'express';
 
 const userRouter = express.Router();
 
-import { signup , getUser } from '../controllers/userController.js';
+import { signup , getUser, updateUser } from '../controllers/userController.js';
 
 userRouter.post('/signup', signup);
 userRouter.get('/user/:id', getUser);
+userRouter.put('/user/:id', updateUser);
 
 export default userRouter;
