f9be3a4c9b0da25e85ee4b6f65ed157fe213865f
Banja David Lotigo <innocdavid@gmail.com>
2023-10-04 11:08:51 +0300

    modified - add mongodb id validation

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,12 +1,14 @@
 import express from 'express';
 
+import validateObjectId from '../middleware/validateObjectId.js';
+
 const userRouter = express.Router();
 
 import { signup , getUser, updateUser, deleteUser } from '../controllers/userController.js';
 
 userRouter.post('/signup', signup);
-userRouter.get('/user/:id', getUser);
-userRouter.put('/user/:id', updateUser);
-userRouter.delete('/user/:id', deleteUser);
+userRouter.get('/user/:id', validateObjectId, getUser);
+userRouter.put('/user/:id', validateObjectId, updateUser);
+userRouter.delete('/user/:id', validateObjectId, deleteUser);
 
 export default userRouter;
