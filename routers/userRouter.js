27f48d4c0e4998e3b2e4e0833aef7ff9a78cea99
Banja David Lotigo <innocdavid@gmail.com>
2023-10-09 18:24:30 +0300

    feature - add token verification and refresh routes

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,5 +1,7 @@
 import express from 'express';
 
+import verifyToken from '../middleware/verifyToken.js';
+import refreshToken from '../middleware/refreshToken.js';
 import validateObjectId from '../middleware/validateObjectId.js';
 
 const userRouter = express.Router();
@@ -8,8 +10,9 @@ import { signup , getUser, updateUser, deleteUser, login } from '../controllers/
 
 userRouter.post('/signup', signup);
 userRouter.post('/login', login);
-userRouter.get('/user/:id', validateObjectId, getUser);
-userRouter.put('/user/:id', validateObjectId, updateUser);
-userRouter.delete('/user/:id', validateObjectId, deleteUser);
+userRouter.get('/user/:id', verifyToken, validateObjectId, getUser);
+userRouter.put('/user/:id', verifyToken, validateObjectId, updateUser);
+userRouter.delete('/user/:id', verifyToken, validateObjectId, deleteUser);
+userRouter.get('/refresh', refreshToken, verifyToken, getUser);
 
 export default userRouter;
