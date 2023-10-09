85f4467e31ee35aa9ca3b8957c695eeb46c74221
Banja David Lotigo <innocdavid@gmail.com>
2023-10-09 12:11:35 +0300

    feature - add user login route

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -4,9 +4,10 @@ import validateObjectId from '../middleware/validateObjectId.js';
 
 const userRouter = express.Router();
 
-import { signup , getUser, updateUser, deleteUser } from '../controllers/userController.js';
+import { signup , getUser, updateUser, deleteUser, login } from '../controllers/userController.js';
 
 userRouter.post('/signup', signup);
+userRouter.post('/login', login);
 userRouter.get('/user/:id', validateObjectId, getUser);
 userRouter.put('/user/:id', validateObjectId, updateUser);
 userRouter.delete('/user/:id', validateObjectId, deleteUser);
