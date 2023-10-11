e0f1cb209ad145b4d433179716b5cc06ae99a22d
Banja David Lotigo <innocdavid@gmail.com>
2023-10-11 08:17:52 +0300

    modified - remove routes for fetching, updating and deleting a user by their id

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,18 +2,17 @@ import express from 'express';
 
 import verifyToken from '../middleware/verifyToken.js';
 import refreshToken from '../middleware/refreshToken.js';
-import validateObjectId from '../middleware/validateObjectId.js';
 
 const userRouter = express.Router();
 
-import { signup , getUser, updateUser, deleteUser, login, logout } from '../controllers/userController.js';
+import { signup, login, logout } from '../controllers/userController.js';
 
-userRouter.post('/signup', signup);
-userRouter.post('/login', login);
-userRouter.post('/logout', verifyToken, logout);
-userRouter.get('/user/:id', verifyToken, validateObjectId, getUser);
-userRouter.put('/user/:id', verifyToken, validateObjectId, updateUser);
-userRouter.delete('/user/:id', verifyToken, validateObjectId, deleteUser);
-userRouter.get('/refresh', refreshToken, verifyToken, getUser);
+userRouter.post('/user/signup', signup);
+userRouter.post('/user/login', login);
+userRouter.post('/user/logout', verifyToken, logout);
+// userRouter.get('/user/:id', verifyToken, validateObjectId, getUser);
+// userRouter.put('/user/:id', verifyToken, validateObjectId, updateUser);
+// userRouter.delete('/user/:id', verifyToken, validateObjectId, deleteUser);
+userRouter.get('/user/refresh', refreshToken, verifyToken);
 
 export default userRouter;
