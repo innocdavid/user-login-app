20d68369bef0a9f603c93770ad9bbc58d5b3d549
Banja David Lotigo <innocdavid@gmail.com>
2023-10-11 07:58:24 +0300

    modified - change to approach to tests

--- tests/test.js
+++ tests/test.js
@@ -2,30 +2,49 @@ import request from 'supertest';
 import app from '../app.js';
 
 describe('User Controller', () => {
-    let user1;
-    describe('POST /api/v1/signup', () => {
-        it('should add one user', async () => {
-          const res = await request(app).post('/signup').send({
-            firstName: 'test',
-            lastName: 'user',
-            email: 'user2@example.com',
-            password: '12345678',
-          });
-          console.log(res.body)
-          expect(res.statusCode).toBe(201);
-          expect(res.body).toHaveProperty('id');
-          expect(res.body.email).toBe('user1@example.com');
-          user1 = res.body;
-        }, 10000); // Set a timeout of 10 seconds (10000 milliseconds)
+  let user = {};
+  
+  beforeEach(async () => {
+      // Run this code before each test
+      const res = await request(app).post('/api/v1/signup').send({
+          firstName: 'Google',
+          lastName: 'Bard',
+          email: 'bard@google.com',
+          password: '12345678',
       });
-      
-    // describe('GET /api/v1/user/:id', () => {
-    //     it('should return one user', async () => {
-    //       const res = await request(server).get(`/api/v1/user/${user1.id}`);
-    //       expect(res.statusCode).toBe(200);
-    //       expect(res.body).toHaveProperty('id');
-    //     });
-    // });    
+      user = res; // Update the user object
+  });
 
-});  
+  describe('POST /api/v1/signup', () => {
+    it('should add one user', async () => {
+      expect(user.statusCode).toBe(201);
+      expect(user.body).toHaveProperty('id');
+    });
+  });
+
+  describe('POST /api/v1/login', () => {
+    it('should login', async () => {
+      const beforeLoginDate = new Date();
+      const res = await request(app).post('/api/v1/login').send({
+        email: 'bard@google.com',
+        password: '12345678',
+      });
+      console.log(res)
+      expect(res.statusCode).toBe(201)  
+      // expect(new Date(res.body.lastLogin).getTime()).toBeGreaterThanOrEqual(beforeLoginDate.getTime());
+    });
+  })
+
+
+  // describe('GET /api/v1/user/:id', () => {
+  //   it('should return one user', async () => {
+  //     // Now, the user object is set before running this test
+  //     const res = await request(app).get(`/api/v1/user/${user.body.id}`);
+  //     console.log('user', res)
+  //     expect(res.statusCode).toBe(200); // Should be 200 for successful retrieval
+  //     expect(res.body).toHaveProperty('id');
+  //   });
+  // });
+});
+
 
